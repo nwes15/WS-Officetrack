3f095beaa0ba41e17320f0df1a363f9cce2f8e80
Wesley Novais <109444226+nwes15@users.noreply.github.com>
2025-03-06 16:33:23 -0300

    Update middleware.py

--- middleware.py
+++ middleware.py
@@ -6,41 +6,53 @@ app = Flask(__name__)
 
 @app.route('/busca-cep', methods=['POST'])
 def busca_cep():
-    # Recebe a requisição SOAP e faz o parsing
+    # Recebe a requisição SOAP em XML
     soap_request = request.data
-    
-    # Converte o XML de entrada para dicionário
+
     try:
+        # Faz o parsing do XML recebido
         data = xmltodict.parse(soap_request)
-        cep = data['soapenv:Envelope']['soapenv:Body']['web:consultaCep']['cep']
-    except KeyError:
+        
+        # Extraindo o CEP do XML (agora vem no campo <Value> dentro de <Field>)
+        cep = None
+        for field in data['soapenv:Envelope']['soapenv:Body']['web:consultaCep']['Field']:
+            if field['Id'] == 'CEP':
+                cep = field['Value']
+                break
+
+    except (KeyError, xmltodict.expat.ExpatError):
         return Response("<error>Formato SOAP inválido</error>", status=400, content_type='text/xml')
 
     if not cep:
         return Response("<error>CEP não informado</error>", status=400, content_type='text/xml')
 
     try:
-        # Acessa o ViaCEP para buscar as informações do CEP
+        # Fazendo a requisição ao ViaCEP no formato XML
         response = requests.get(f'https://viacep.com.br/ws/{cep}/xml/')
-        dados = response.text
+        dados = xmltodict.parse(response.text)
 
         # Se o CEP não for encontrado, retornamos erro
-        if "<erro>" in dados:
+        if 'erro' in dados['xmlcep']:
             return Response("<error>CEP não encontrado</error>", status=404, content_type='text/xml')
 
-        # Formata a resposta com as informações desejadas no formato XML
-        return Response(f"""
-        <?xml version="1.0" encoding="UTF-8"?>
-        <response>
-            <LOGRADOURO>{dados['logradouro']}</LOGRADOURO>
-            <COMPLEMENTO>{dados['complemento']}</COMPLEMENTO>
-            <BAIRRO>{dados['bairro']}</BAIRRO>
-            <CIDADE>{dados['localidade']}</CIDADE>
-            <ESTADO>{dados['uf']}</ESTADO>
-        </response>
-        """, content_type='text/xml')
+        # Montando a resposta SOAP em XML para o Officetrack
+        resposta_xml = f"""<?xml version="1.0" encoding="UTF-8"?>
+        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+            <soapenv:Body>
+                <response>
+                    <LOGRADOURO>{dados['xmlcep']['logradouro']}</LOGRADOURO>
+                    <COMPLEMENTO>{dados['xmlcep']['complemento']}</COMPLEMENTO>
+                    <BAIRRO>{dados['xmlcep']['bairro']}</BAIRRO>
+                    <CIDADE>{dados['xmlcep']['localidade']}</CIDADE>
+                    <ESTADO>{dados['xmlcep']['uf']}</ESTADO>
+                </response>
+            </soapenv:Body>
+        </soapenv:Envelope>
+        """
 
-    except requests.exceptions.RequestException as e:
+        return Response(resposta_xml, content_type='text/xml')
+
+    except requests.exceptions.RequestException:
         return Response("<error>Erro ao buscar CEP</error>", status=500, content_type='text/xml')
 
 if __name__ == '__main__':
