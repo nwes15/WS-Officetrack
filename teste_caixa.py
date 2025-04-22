08611ea1221c4f4c85d089e55acc82a81b4a390e
Wesley Novais <nwesley1500@gmail.com>
2025-04-22 12:27:47 -0300

    fix12

--- teste_caixa.py
+++ teste_caixa.py
@@ -76,28 +76,28 @@ def gerar_resposta_string_template(peso_novo, pesobalanca_novo, balanca_id, tstp
     <Fields>
       <TableField>
         <ID>{tabela_id_resp}</ID>
-        <OverrideData>0</OverrideData>
+        <OverrideData>1</OverrideData>
         <Rows>
           <Row>
             <Fields>
               <Field>
                 <ID>{tstpeso_id_resp}</ID>
-                <OverrideData>0</OverrideData>
+                <OverrideData>1</OverrideData>
                 <Value>{tstpeso_valor_usado}</Value>
               </Field>
               <Field>
                 <ID>{peso_id_resp}</ID>
-                <OverrideData>0</OverrideData>
+                <OverrideData>1</OverrideData>
                 <Value>{peso_novo}</Value>
               </Field>
               <Field>
                 <ID>{pesobalanca_id_resp}</ID>
-                <OverrideData>0</OverrideData>
+                <OverrideData>1</OverrideData>
                 <Value>{pesobalanca_novo}</Value>
               </Field>
               <Field>
                 <ID>WS</ID>
-                <OverrideData>0</OverrideData>
+                <OverrideData>1</OverrideData>
                 <Value>Pressione Lixeira para nova consulta</Value>
                 </Field>
             </Fields>
