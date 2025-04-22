dc67e9b35672dc49805ed3daded79e7701cc96d3
Wesley Novais <nwesley1500@gmail.com>
2025-04-22 10:34:36 -0300

    fix2

--- teste_caixa.py
+++ teste_caixa.py
@@ -80,17 +80,14 @@ def gerar_resposta_string_template(peso_novo, pesobalanca_novo, balanca_id, tstp
             <Fields>
               <Field>
                 <ID>{tstpeso_id_resp}</ID>
-                <OverrideData>0</OverrideData>
                 <Value>{tstpeso_valor_usado}</Value>
               </Field>
               <Field>
                 <ID>{peso_id_resp}</ID>
-                <OverrideData>0</OverrideData>
                 <Value>{peso_novo}</Value>
               </Field>
               <Field>
                 <ID>{pesobalanca_id_resp}</ID>
-                <OverrideData>0</OverrideData>
                 <Value>{pesobalanca_novo}</Value>
               </Field>
             </Fields>
