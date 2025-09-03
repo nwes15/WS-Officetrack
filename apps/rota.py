0f63313de6a4632017f974b39a22f0f980b64e0a
Wesley Novais <109444226+nwes15@users.noreply.github.com>
2025-09-03 10:52:38 -0300

    novo app

---
+++ apps/rota.py
@@ -0,0 +1,31 @@
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+@app.route('/simple-xml')
+def simple_xml():
+    xml_data = '''
+    <Response>
+     <Message>
+     <Text>Invalid data</Text>
+     <Icon>Info</Icon>
+     </Message>
+     <ReturnValue>
+     <Items>
+     <Item>
+     <Text>One</Text>
+     <Value>1</Value>
+     </Item>
+     <Item>
+     <Text>Two</Text>
+     <Value>2</Value>
+     </Item>
+     <Item>
+     <Text>Three</Text>
+     <Value>3</Value>
+     </Item>
+     </Items>
+     </ReturnValue>
+    </Response>
+    '''
+    return Response(xml_data.encode('utf-16'), mimetype='application/xml')
